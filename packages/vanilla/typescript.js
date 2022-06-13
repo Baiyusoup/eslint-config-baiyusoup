300532af188b2c2c60f5c855e747cb80eec67e49
baiyusoup <1575727840@qq.com>
2022-06-13 11:01:59 +0800

    feat: update semi/member-delimiter-style/brace-style

--- packages/vanilla/typescript.js
+++ packages/vanilla/typescript.js
@@ -1,15 +1,13 @@
-// Anthony Fu 的typescript eslint config. 
+// Anthony Fu 的typescript eslint config.
 // https://github.com/antfu/
 
 module.exports = {
-  extends: ["@antfu/eslint-config-ts"],
+  extends: ['@antfu/eslint-config-ts'],
   rules: {
-    'semi': ['error', 'always', {
-      omitLastInOneLineBlock: true,
-    }],
-    '@typescript-eslint/semi': ['error', 'always', {
-      omitLastInOneLineBlock: true,
-    }],
+    'semi': ['error', 'always', { omitLastInOneLineBlock: true }],
+    '@typescript-eslint/semi': ['error', 'always', { omitLastInOneLineBlock: true }],
     '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'semi' } }],
-  }
+    'brace-style': ['error', '1tbs'],
+    '@typescript-eslint/brace-style': ['error', '1tbs'],
+  },
 }
